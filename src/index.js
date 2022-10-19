64ec25fb5ad107f39c200cb11a8bd2ec4e270148
Matt Huntsberry <matt.huntsberry@nielsen.com>
2022-10-18 20:14:23 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// ESM - ECMASCRIPT MODULES
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+// Common JS Import Syntax
+// const express = require("express");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
